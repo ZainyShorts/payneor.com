aef8b5bfb8e736b1f137ff71d06ad2d6b055dccf
“Zainy <devzain170@gmail.com>
2025-12-29 13:43:30 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+'use client'
 import { Navbar } from "@/components/navbar"
 import { Hero } from "@/components/hero" 
 import PaymentSolutionsWithBlogs from "@/components/solutions"
 import { ValueServices } from "@/components/value-services"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <main className="relative">
       <Navbar />
       <Hero /> 
+      <Suspense fallback={<div>Loading...</div>}>
       <PaymentSolutionsWithBlogs/> 
+      </Suspense>
       <ValueServices />
     </main>
   )
